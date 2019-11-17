74fb988e6535a51ca9480be54fc5e21cb4d368f6
s-shiki <37012360+s-shiki@users.noreply.github.com>
2019-11-17 13:14:37 +0900

    added func generateHTML

--- utils.go
+++ utils.go
@@ -16,3 +16,12 @@ func session(w http.ResponseWriter, r *http.Request) (sess data.Session, err err
   }
   return
 }
+
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+  var files []string
+  for _, file := range filenames {
+    files = append(files, fmt.Sprintf("templates/%s.html", file))
+  }
+  templates := template.Must(template.ParseFiles(files...))
+  templates.ExecuteTemplate(writer, "layout", data)
+}
